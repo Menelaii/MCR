74c4ee2f66dcdffb9f45609c52d68636825b07f9
Menelaii <78534773+Menelaii@users.noreply.github.com>
2021-05-24 21:40:58 +0400

    2.2

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -7,10 +7,19 @@ using UnityEngine.UI;
 public class GameOverMenu : MonoBehaviour
 {
     [SerializeField] private Button _respawn;
+    [SerializeField] private KeyCode _respawnKey = KeyCode.R;
+    [SerializeField] private Text _text;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(_respawnKey))
+            OnRespawnButtonClick();
+    }
 
     public void OnPlayerDied()
     {
         gameObject.SetActive(true);
+        _text.text = "Or press " + _respawnKey + " button";
         _respawn.onClick.AddListener(OnRespawnButtonClick);
     }
 
