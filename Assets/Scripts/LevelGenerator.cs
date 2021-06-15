1029daab96fc81740397b1e041a0db35e766d680
Menelaii <78534773+Menelaii@users.noreply.github.com>
2021-06-15 21:38:17 +0300

    Generator fix

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -9,16 +9,15 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private int _roomsOnStart;
     [SerializeField] private int _roomsPerTick;
     [SerializeField] private int _roomsToIncreaseDifficulty;
-    [SerializeField] private int _roomsToBonus;//спавн бонусных комнат пока не реализован
     [SerializeField] private RoomsContainer _container;
-    
+    [SerializeField] private Room _lastRoom;
+
     private Room[] _rooms;
     private List<Room> _spawnList;
     private float _roomHeight;
     private int _spawnedRooms;
     private int _difficulty;
-    private int _currentDifficulty;
-    private bool _playerHaveSword;//изменение состояния пока не реализовано
+    private int _maxCurrentDifficultyOfRooms;
 
     private void Awake()
     {
@@ -26,32 +25,37 @@ public class LevelGenerator : MonoBehaviour
         _rooms = _container.GetRooms();
         _spawnList = new List<Room>();
         _difficulty = _startDifficulty;
-        _currentDifficulty = _difficulty;
+        _maxCurrentDifficultyOfRooms = _difficulty;
         SpawnRooms(_roomsOnStart);
     }
-
-    public void OnLadderPassed()
+    private void SpawnRooms(int count)
     {
-        if (_spawnedRooms >= _roomsToIncreaseDifficulty)
+        for (int i = 0; i < count; i++)
         {
-            _difficulty++;
-            _roomsToIncreaseDifficulty += _roomsToIncreaseDifficulty;
+            TryReformSpawnList();
+            SpawnRoomFromList();
         }
-
-        SpawnRooms(_roomsPerTick);
     }
-
     private void TryReformSpawnList()
     {
-        if (_spawnList.Count == 0 || _currentDifficulty != _difficulty)
+        if (_spawnList.Count == 0 || _maxCurrentDifficultyOfRooms < _difficulty)
         {
             ReformSpawnList();
         }
     }
+    private void SpawnRoomFromList()
+    {
+        Room roomToSpawn = _spawnList[0];
+        _spawnList.RemoveAt(0);
 
+        Vector3 spawnPosition = Vector3.up * (_spawnedRooms + 1) * _roomHeight;
+        _lastRoom = Instantiate(roomToSpawn, spawnPosition, Quaternion.identity);
+
+        _spawnedRooms++;
+    }
     private void ReformSpawnList()
     {
-        _currentDifficulty = _difficulty;
+        _maxCurrentDifficultyOfRooms = _difficulty;
         _spawnList.Clear();
         for (int i = 0; i < _rooms.Length; i++)
         {
@@ -61,19 +65,13 @@ public class LevelGenerator : MonoBehaviour
         ShuffleSpawnList();
         SortSpawnList();
     }
-
     private void TryAddToSpawnList(Room room)
     {
-        if (room.NeedSword && _playerHaveSword == false)
-        {
-            return;
-        }
-        else if(_currentDifficulty >= room.Difficulty)
+        if(_maxCurrentDifficultyOfRooms >= room.Difficulty)
         {
             _spawnList.Add(room);
         }
     }
-
     private void ShuffleSpawnList()
     {
         for (int i = _spawnList.Count - 1; i >= 1; i--)
@@ -84,7 +82,6 @@ public class LevelGenerator : MonoBehaviour
             _spawnList[i] = temp;
         }
     }
-
     private void SortSpawnList()
     {
         var sortedList = new List<Room>(_spawnList.Count);
@@ -93,39 +90,48 @@ public class LevelGenerator : MonoBehaviour
 
         var leftLadderRooms = _spawnList.Where(room => room.IsLadderOnRight == false).ToArray();
 
+        int evenCounter = 0;
+        int oddCounter = 0;
         for (int i = 0; i < _spawnList.Count; i++)
         {
-            int evenCounter = 0;
-            int oddCounter = 0;
-            if(i % 2 == 0)
+            if (_lastRoom.IsLadderOnRight)
             {
-                sortedList.Add(leftLadderRooms[evenCounter]);
-                evenCounter++;
+                if (i % 2 == 0)
+                {
+                    sortedList.Add(leftLadderRooms[evenCounter]);
+                    evenCounter++;
+                }
+                else
+                {
+                    sortedList.Add(rightLadderRooms[oddCounter]);
+                    oddCounter++;
+                }
             }
             else
             {
-                sortedList.Add(rightLadderRooms[oddCounter]);
-                oddCounter++;
+                if (i % 2 == 0)
+                {
+                    sortedList.Add(rightLadderRooms[oddCounter]);
+                    oddCounter++;
+                }
+                else
+                {
+                    sortedList.Add(leftLadderRooms[evenCounter]);
+                    evenCounter++;
+                }
             }
         }
+
         _spawnList = sortedList;
     }
-
-    private void SpawnRoomFromList()
+    public void OnLadderPassed()
     {
-        Room roomToSpawn = _spawnList[0];
-        _spawnList.RemoveAt(0);
-        Vector3 spawnPosition = Vector3.up * (_spawnedRooms + 1) * _roomHeight;
-        Instantiate(roomToSpawn, spawnPosition, Quaternion.identity);
-        _spawnedRooms++;
-    }
-
-    private void SpawnRooms(int count)
-    {
-        for (int i = 0; i < count; i++)
+        if (_spawnedRooms >= _roomsToIncreaseDifficulty)
         {
-            TryReformSpawnList();
-            SpawnRoomFromList();
+            _difficulty++;
+            _roomsToIncreaseDifficulty += _roomsToIncreaseDifficulty;
         }
+
+        SpawnRooms(_roomsPerTick);
     }
 }
