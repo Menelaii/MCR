ba11944822611b7ef1e9aa336916fd790bcdfbc8
Menelaii <78534773+Menelaii@users.noreply.github.com>
2021-06-16 14:30:09 +0300

    5.1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Enemy : MonoBehaviour, IInteractableWithSword, ISleeping
 {
     [SerializeField] private WayPointMovement _movementScript;
     [SerializeField] private float _awakenTime;
+    [SerializeField] private bool _destroySwordOnInteract;
+    [SerializeField] private Color _acid;
+
+    private void Start()
+    {
+        if (_destroySwordOnInteract)
+            GetComponent<SpriteRenderer>().color = _acid;
+    }
 
     public void Interact(Sword sword) 
     {
+        if (_destroySwordOnInteract)
+        {
+            sword.gameObject.SetActive(false);
+        }
+
         Destroy(gameObject);
     }
 
